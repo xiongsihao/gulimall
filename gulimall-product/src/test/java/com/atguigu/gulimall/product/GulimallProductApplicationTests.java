f0b8dba914edf4c979655a5a55237894e771a166
xsh <xsh2668028614@qq.com>
2021-05-12 00:39:21 +0800

    项目初始化

---
+++ gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
@@ -0,0 +1,16 @@
+package com.atguigu.gulimall.product;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GulimallProductApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
